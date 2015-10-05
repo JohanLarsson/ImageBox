1029f97379996dcfc68c58e53fd4328b92f84097
Johan Larsson <johan_gavle1@hotmail.com>
2015-10-05 21:19:02 +0200

    Initial

---
+++ ImageBox/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ImageBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
